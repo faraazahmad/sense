dda21ec48f3ffe289b77524dfda443720a03935a
Syed Faraaz Ahmad <faraaz98@live.com>
2018-12-01 14:00:20 +0530

    put 4 articles per column

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,8 +1,8 @@
 require 'news-api'
 
 module ArticleHelper
-    def get_five(order)
-        x = 5 * order
-        @headlines[x..x+4]
+    def get_four(order)
+        x = 4 * order
+        @headlines[x..x+3]
     end
 end
