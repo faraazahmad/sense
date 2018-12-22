a0b53af559980aed8c2f18d908da9acd6f348cf4
Syed Faraaz Ahmad <faraaz98@live.com>
2018-12-22 20:06:27 +0530

    return user created posts <= 6hrs old

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,2 +1,16 @@
 module PostHelper
+    # Return all user created posts which are
+    # less than 6 hours old
+    def current_posts
+        current_hour = Time.now.hour
+        posts = []
+
+        Post.all.each do |p|
+            if p.created_at.hour - current_hour <= 6
+                posts << p
+            end
+        end
+
+        return posts
+    end
 end
